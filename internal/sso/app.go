7cddceb2d2965b013e9d19115e4b2c795f083811
kalex <alextek2010@gmail.com>
2023-12-11 00:07:45 +0300

    [MODIFY][NOT WORKING] added launcher in cmd folder, have problems with dependencies in go.mod file need to fix it

---
+++ internal/sso/app.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"log/slog"
+
+	authgrpc "bank/internal/grpc/auth"
+
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func New(
+	log *slog.Logger,
+	port int,
+) *App {
+	gRPCServer := grpc.NewServer()
+
+	authgrpc.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
